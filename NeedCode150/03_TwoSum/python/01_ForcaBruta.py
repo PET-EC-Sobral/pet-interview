763c05d653cce7adae996a2b32082d9429f4d8ff
P4m3l4m4r14 <pamelamaria@alu.ufc.br>
2026-04-29 23:39:49 -0300

    add TwoSum solutions

---
+++ NeedCode150/03_TwoSum/python/01_ForcaBruta.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Solução força bruta
+        tamanho = len(nums)
+
+        for i in range(tamanho):
+            for j in range(i + 1, tamanho):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
